b2eef0247b7557a116d8efb3d7e83aaaf352cc30
Aman <devproddeploy@gmail.com>
2020-07-26 00:13:21 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { HelmetProvider, Helmet } from 'react-helmet-async'
+
+const useLocalState = (key, defaultValue) => {
+
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue === null ? defaultValue : JSON.parse(storedValue);
+  });
+
+  useEffect(() => {
+    const listener = (e) => {
+      if (e.storageArea === localStorage && e.key === key) {
+        setValue(JSON.parse(e.newValue));
+      }
+    };
+    window.addEventListener("storage", listener);
+
+    return () => {
+      window.removeEventListener("storage", listener);
+    };
+  }, [key]);
+
+  const setValueInLocalStorage = (newValue) => {
+    setValue((currentValue) => {
+      const res = typeof newValue === 'function' ? newValue(currentValue) : newValue;
+      localStorage.setItem(key, JSON.stringify(res));
+      return res;
+    })
+  }
+
+  return [value, setValueInLocalStorage]
+}
 
 function App() {
+
+  const [username, setUsername] = useLocalState('theme', '');
+  const [theme, setTheme] = useLocalState('theme', 'dark');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelmetProvider>
+        <Helmet>
+          <body data-theme={theme} />
+        </Helmet>
+      <button onClick={e => setTheme(curr => curr === 'light' ? 'dark': 'light')}>{theme}</button>
+      <form>
+        <div className="input-field">
+          <input type="text" value={username} onChange={e => setUsername(e.target.value)}/>
+        </div>
+      </form>
+      </HelmetProvider>
     </div>
   );
 }
